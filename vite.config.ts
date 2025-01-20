b69d82e37f7817fd329706b0ca6819c9f984f15f
ongheong <pudding4271@gmail.com>
2025-01-20 21:46:40 +0900

    chore: vite.config.ts에 alias 경로, 개발 서버 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src'},
+      { find: '@assets', replacement: '/src/assets'},
+      { find: '@common', replacement: '/src/common'},
+      { find: '@mocks', replacement: '/src/mocks'},
+      { find: '@pages', replacement: '/src/pages'},
+      { find: '@service', replacement: '/src/service'},
+      { find: '@stores', replacement: '/src/stores'},
+    ],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+});
