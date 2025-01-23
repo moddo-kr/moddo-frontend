f8a0ecdb4716fadbb7765cb0edc7109a60466618
ongheong <pudding4271@gmail.com>
2025-01-23 13:46:10 +0900

    chore: eslint 비활성화 규칙 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,8 @@ module.exports = {
     ecmaFeatures: {
       jsx: true, // JSX 지원
     },
-    project: './tsconfig.json',
+    project: ['./tsconfig.app.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
   plugins: [
     'react',
@@ -36,10 +37,38 @@ module.exports = {
     'jsx-a11y/anchor-is-valid': 'off',
     'react/jsx-props-no-spreading': 'off',
     'prettier/prettier': ['error'],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.test.js', // 테스트 파일 허용
+          '**/*.test.ts', // 테스트 파일 허용
+          '**/*.test.tsx', // 테스트 파일 허용
+          '**/*.stories.js', // Storybook 파일 허용
+          '**/*.stories.tsx', // Storybook 파일 허용
+          '**/*.stories.ts', // Storybook 파일 허용
+          'src/setupTests.ts', // 테스트 설정 파일 허용
+        ],
+      },
+    ],
+    'react/require-default-props': 'off', // defaultProps 사용을 권장하지 않음
+    'react/no-unescaped-entities': 'off', // 문자열 내에서 특수문자를 이스케이프하지 않아도 됨
   },
   settings: {
     react: {
       version: 'detect', // React 버전 자동 감지
     },
   },
+  overrides: [
+    {
+      files: ['.eslintrc.cjs'], // .eslintrc.cjs 파일에 대한 규칙 비활성화
+      parserOptions: {
+        project: null, // TypeScript 타입 검사 비활성화
+      },
+      rules: {
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
 };
