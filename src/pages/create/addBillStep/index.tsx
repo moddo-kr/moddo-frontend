dc38b9d8f5bd6a1133319ac8df808d5081c7f845
yoouyeon <jyeon.yoon59@gmail.com>
2025-01-30 00:38:32 +0900

    design: 기본 입력 필드 추가

--- src/pages/create/addBillStep/index.tsx
+++ src/pages/create/addBillStep/index.tsx
@@ -22,6 +22,21 @@ function AddBillStep() {
             </S.BillFormFieldLabel>
             <S.BillFormFieldInput />
           </S.BillFormField>
+          <S.BillFormField required>
+            <S.BillFormFieldLabel>
+              지출 장소 및 내용
+              <S.BillFormFieldRequired>*</S.BillFormFieldRequired>
+            </S.BillFormFieldLabel>
+            <S.BillFormFieldInput placeholder="ex. 투썸플레이스" />
+          </S.BillFormField>
+          <S.BillFormField>
+            <S.BillFormFieldLabel>지출일</S.BillFormFieldLabel>
+            <S.BillFormFieldInput />
+          </S.BillFormField>
+          <S.BillFormField>
+            <S.BillFormFieldLabel>참여자</S.BillFormFieldLabel>
+            <S.BillFormFieldInput />
+          </S.BillFormField>
         </S.BillFormCard>
       </S.BillListWrapper>
     </VStack>
