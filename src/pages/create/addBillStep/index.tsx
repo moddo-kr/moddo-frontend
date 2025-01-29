a930e847fed6ac50a067610e6171b5c2135f382e
yoouyeon <jyeon.yoon59@gmail.com>
2025-01-30 00:32:01 +0900

    design: 지출 입력 카드 기본 디자인 추가

--- src/pages/create/addBillStep/index.tsx
+++ src/pages/create/addBillStep/index.tsx
@@ -1,8 +1,10 @@
+import { VStack } from '@chakra-ui/react';
 import * as S from './styles/index.styles';
 
 function AddBillStep() {
   return (
-    <div>
+    <VStack align="flex-start">
+      {/* TODO : VStack: 정산 시작 step 공통 레이아웃 적용 전 임시 레이아웃 */}
       <S.Header>
         <S.HeaderButton>지출 추가</S.HeaderButton>
       </S.Header>
@@ -10,7 +12,19 @@ function AddBillStep() {
         <S.MoimName>모임 이름</S.MoimName>
         <S.TopMessage>{`의\n지출 내역을 입력해주세요.`}</S.TopMessage>
       </S.TopWrapper>
-    </div>
+      <S.BillListWrapper>
+        <S.BillFormCard>
+          <S.BillFormCardTitle>1차</S.BillFormCardTitle>
+          <S.BillFormField required>
+            <S.BillFormFieldLabel>
+              결제 금액
+              <S.BillFormFieldRequired>*</S.BillFormFieldRequired>
+            </S.BillFormFieldLabel>
+            <S.BillFormFieldInput />
+          </S.BillFormField>
+        </S.BillFormCard>
+      </S.BillListWrapper>
+    </VStack>
   );
 }
 
