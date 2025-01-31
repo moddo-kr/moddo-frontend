0d56e5a650afb4c4d8dacb288df884c6bc74d70c
ongheong <pudding4271@gmail.com>
2025-02-01 01:29:08 +0900

    chore: selectGroup 페이지 svg 아이콘 추가 및 svgr 적용

---
+++ src/assets/svgs/SelectRecentGroupIcon.tsx
@@ -0,0 +1,19 @@
+import type { SVGProps } from 'react';
+const SvgSelectRecentGroupIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 44 44"
+    {...props}
+  >
+    <circle cx={22} cy={22} r={22} fill="#CECFD3" />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.6}
+      d="m16 22 3.995 4L28 18"
+    />
+  </svg>
+);
+export default SvgSelectRecentGroupIcon;
