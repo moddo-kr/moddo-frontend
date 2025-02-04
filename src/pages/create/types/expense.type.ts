7c9f4f2f584c873ed57b2fed049e36a802bbfaec
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-05 02:56:48 +0900

    feat: 지출 관련 타입 정의

---
+++ src/pages/create/types/expense.type.ts
@@ -0,0 +1,25 @@
+import * as z from 'zod';
+
+const ExpenseMemberSchema = z.object({
+  id: z.number().int().positive(), // 회원 고유번호
+  amount: z.number().int().positive(), // 회원별 정산 금액
+  name: z.string(), // 회원 이름
+});
+
+const ExpenseSchema = z.object({
+  amount: z.number().int().positive(), // 결제 금액
+  content: z.string().min(1), // 지출 장소 및 내용
+  date: z.date(), // 지출일
+  memberExpenses: z.array(ExpenseMemberSchema).min(1), // 참여자
+});
+
+export const ExpenseFormSchema = z.object({
+  expenses: z.array(ExpenseSchema),
+});
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ExpenseMember extends z.infer<typeof ExpenseMemberSchema> {}
+
+export interface Expense extends z.infer<typeof ExpenseSchema> {
+  memberExpenses: ExpenseMember[];
+}
