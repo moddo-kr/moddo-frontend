00f76369202b9b2063978794931830de0f97cc42
ongheong <pudding4271@gmail.com>
2025-02-05 15:20:47 +0900

    chore: Layout 컴포넌트에서 스타일 코드 분리

--- src/common/components/layout/index.tsx
+++ src/common/components/layout/index.tsx
@@ -1,20 +1,7 @@
-import styled from 'styled-components';
+import * as S from './index.styles';
 
 function Layout({ children }: { children: React.ReactNode }) {
-  return <Wrapper>{children}</Wrapper>;
+  return <S.Wrapper>{children}</S.Wrapper>;
 }
-
-const Wrapper = styled.div`
-  overflow-x: hidden;
-  max-width: 600px;
-  min-width: 320px;
-  height: auto;
-  width: 100%;
-  background-color: white;
-  min-height: 100dvh;
-  display: flex;
-  flex-direction: column;
-  margin: 0 auto;
-`;
 
 export default Layout;
