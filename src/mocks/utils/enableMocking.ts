2e029157f1ddb57c20afdec1cca35176f7e986f1
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-06 09:32:48 +0900

    fix: worker.start()가 종료되지 않던 문제 해결
    - https://mswjs.io/docs/integrations/browser
    - worker를 제대로 import해오지 못하고 있었던 것 같다...

--- src/mocks/utils/enableMocking.ts
+++ src/mocks/utils/enableMocking.ts
@@ -1,9 +1,10 @@
-import { worker } from '../browser';
-
 async function enableMocking() {
   if (process.env.NODE_ENV !== 'development') {
     return;
   }
+
+  const { worker } = await import('../browser');
+
   // eslint-disable-next-line consistent-return
   return worker.start();
 }
