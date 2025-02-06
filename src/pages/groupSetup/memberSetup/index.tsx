eccb6cf36024193f9831ab05d34054be7a305c8a
ongheong <pudding4271@gmail.com>
2025-02-06 21:49:02 +0900

    fix: MemberSetup페이지 버튼 텍스트 내용 수정, Header에 navigate 추가

--- src/pages/groupSetup/memberSetup/index.tsx
+++ src/pages/groupSetup/memberSetup/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex} from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 import { useNavigate } from 'react-router';
 import * as S from '../index.styles';
 import Header from '@/common/components/Header';
@@ -15,7 +15,12 @@ function MemberSetup() {
 
   return (
     <>
-      <Header title="" showIcon={true} type="TitleLeft" />
+      <Header
+        title=""
+        showIcon={true}
+        type="TitleLeft"
+        handleBackButtonClick={() => navigate(-1)}
+      />
       <Flex
         direction="column"
         justify="space-between"
@@ -27,14 +32,18 @@ function MemberSetup() {
       >
         <Flex direction="column">
           <S.TitleText>
-            함께한 참가자를
+            모임에 함께한
             <br />
-            추가해주세요.
+            참여자를 추가해주세요.
           </S.TitleText>
           <AddMember />
         </Flex>
-        <Button height={12} borderRadius={12} onClick={() => navigate(ROUTE.create)}>
-          다음
+        <Button
+          height={12}
+          borderRadius={12}
+          onClick={() => navigate(ROUTE.create)}
+        >
+          정산 시작!
         </Button>
       </Flex>
     </>
