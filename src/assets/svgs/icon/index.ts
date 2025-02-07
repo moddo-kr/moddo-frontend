144a097b6c1aa65e23a71379dcb31b6ea949d55e
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-07 23:03:43 +0900

    design: ArrowLeft 아이콘 추가

--- src/assets/svgs/icon/index.ts
+++ src/assets/svgs/icon/index.ts
@@ -1,3 +1,4 @@
+export { default as ArrowLeft } from './ArrowLeft';
 export { default as CarbonEdit } from './CarbonEdit';
 export { default as Close } from './Close';
 export { default as Next } from './Next';
