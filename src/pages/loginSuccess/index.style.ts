7290f6911f6edd8a7e5ecc041a8745d321c038ef
ongheong <pudding4271@gmail.com>
2025-02-08 02:00:30 +0900

    feat: 로그인 성공 페이지 UI 구현

---
+++ src/pages/loginSuccess/index.style.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const TitleWrapper = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.625rem 1.25rem; // 10px 20px
+  margin-top: 3.5rem; // 56px
+  width: 100%;
+`;
+
+export const ImgWrapper = styled.div`
+  width: 100%;
+  height: fit-content;
+  padding: 0 1.5rem; // 24px
+  position: absolute;
+  top: 33dvh;
+`;
+
+export const LogoImg = styled.img`
+  width: 100%;
+  object-fit: contain;
+`;
