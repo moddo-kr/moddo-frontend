85c44471ae4389b92773c0e05ae1f6bca91291e4
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-07 21:59:52 +0900

    feat: moveToNextStep에 전달하는 context를 optional로 변경
    context를 저장하지 않고 다음단계로 이동하는 경우도 있음

--- src/common/types/useFunnel.type.ts
+++ src/common/types/useFunnel.type.ts
@@ -10,7 +10,7 @@ interface UseFunnelProps<T> {
 
 /** Funnel 컴포넌트의 기본 Props */
 interface BaseFunnelStepComponentProps<T> {
-  moveToNextStep?: (newContextData: Partial<T>) => void;
+  moveToNextStep?: (newContextData?: Partial<T>) => void;
   moveToPreviousStep?: () => void;
 }
 
