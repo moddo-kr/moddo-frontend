e43eaabf3a9d37e65dffa73e2df2e290a7dd2f30
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-08 11:50:51 +0900

    chore: 지출 내역 더미데이터를 사용하도록 수정

--- src/pages/createBill/confirmStep/index.tsx
+++ src/pages/createBill/confirmStep/index.tsx
@@ -3,26 +3,49 @@ import { ArrowLeft } from '@/assets/svgs/icon';
 import Header from '@/common/components/Header';
 import { BaseFunnelStepComponentProps } from '@/common/types/useFunnel.type';
 import ExpenseCardList from './components/ExpenseCardList';
-import useGetAllExpense from './hooks/useGetAllExpense';
+// import useGetAllExpense from './hooks/useGetAllExpense';
 import getTotalExpense from '../utils/getTotalExpense';
 import { BillContext } from '../types/billContext.type';
 import * as S from './index.styles';
 
-const DUMMY_MEET_ID = 1;
+// const DUMMY_MEET_ID = 1;
+
+const data = {
+  expenses: [
+    {
+      id: 1,
+      amount: 100000,
+      content: '하이디라오',
+      date: new Date('2025-02-03'),
+      memberExpenses: [
+        {
+          memberId: 1,
+          name: '김반숙',
+          amount: 50000,
+        },
+        {
+          memberId: 2,
+          name: '박완숙',
+          amount: 50000,
+        },
+      ],
+    },
+  ],
+};
 
 interface ConfirmStepProps extends BaseFunnelStepComponentProps<BillContext> {}
 
 function ConfirmStep({ moveToNextStep, moveToPreviousStep }: ConfirmStepProps) {
   // TODO : meetId를 알아내는 로직이 필요함
-  const { data, isLoading } = useGetAllExpense(DUMMY_MEET_ID);
+  // const { data, isLoading } = useGetAllExpense(DUMMY_MEET_ID);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
 
-  if (!data || !data.expenses) {
-    return <div>지출 내역이 없습니다.</div>;
-  }
+  // if (!data || !data.expenses) {
+  //   return <div>지출 내역이 없습니다.</div>;
+  // }
 
   return (
     <>
