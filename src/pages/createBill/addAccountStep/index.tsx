bc2bce21f5bea7bf0dddf89aff85a30222ce9d3b
ongheong <pudding4271@gmail.com>
2025-02-08 11:52:40 +0900

    feat: AddAcounStep UI 구현

--- src/pages/createBill/addAccountStep/index.tsx
+++ src/pages/createBill/addAccountStep/index.tsx
@@ -1,8 +1,83 @@
+import Header from '@/common/components/Header';
+import { TitleText } from '@/pages/groupSetup/index.styles';
+import { Button, Flex, Input, useDisclosure } from '@chakra-ui/react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import BankNameDrawer from './components/BankNameDrawer';
+
 function AddAccountStep() {
+  const navigate = useNavigate();
+  const [bankName, setBankName] = useState<string>('');
+  const [accountNumber, setAccountNumber] = useState<string>('');
+  const { open, onOpen, onClose } = useDisclosure();
+
+  const handleBankInputClick = () => {
+    onOpen();
+  };
+
   return (
-    <div>
-      <h1>AddAccountStep</h1>
-    </div>
+    <>
+      <Header
+        title="뒤로가기"
+        showIcon
+        type="TitleLeft"
+        handleBackButtonClick={() => navigate(-1)}
+      />
+      <Flex
+        direction="column"
+        justify="space-between"
+        mx="5"
+        height="100%"
+        mt="10px"
+        mb="32px"
+        flexGrow={1}
+      >
+        <Flex direction="column">
+          <TitleText>
+            정산 받을 계좌를 <br />
+            입력해주세요.
+          </TitleText>
+          <Flex direction="column" mt="23px">
+            <Input
+              borderRadius={12}
+              placeholder="은행 선택"
+              fontSize={16}
+              py={3}
+              height={12}
+              mb={4}
+              value={bankName}
+              onClick={handleBankInputClick}
+              readOnly
+            />
+            <BankNameDrawer
+              open={open}
+              onClose={onClose}
+              setBankName={setBankName}
+            />
+            <Input
+              borderRadius={12}
+              placeholder="계좌번호를 입력해주세요."
+              fontSize={16}
+              py={3}
+              height={12}
+              value={accountNumber}
+              mb={4}
+              onChange={(e) => setAccountNumber(e.target.value)}
+              type="number"
+              inputMode="numeric"
+            />
+          </Flex>
+        </Flex>
+        <Button
+          height={12}
+          borderRadius={12}
+          onClick={() => navigate('/')}
+          disabled={!bankName || !accountNumber}
+        >
+          다음
+        </Button>
+      </Flex>
+    </>
   );
 }
 
