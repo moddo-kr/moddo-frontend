41eacef75c6e25bd5fdbb34423dc05f51295adcd
ongheong <pudding4271@gmail.com>
2025-02-08 11:53:13 +0900

    feat: BanknameDrawer 구현

---
+++ src/pages/createBill/addAccountStep/components/BankNameDrawer/index.style.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const DrawerHeader = styled.h2`
+  font-size: 1.25rem; // 20px
+  font-weight: 700;
+`;
+
+export const DrawerBody = styled.div`
+  height: 370px;
+  overflow-y: auto;
+  gap: 8px;
+  flex-wrap: wrap;
+  display: flex;
+`;
+
+export const SubmitButton = styled.button`
+  width: 100%;
+  padding: 1rem 0.9rem; // 16px 14px
+  border-radius: 2rem; // 32px
+  background-color: black;
+  color: white;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 1.5;
+  margin-bottom: 0.9rem; // 14px
+  cursor: pointer;
+`;
+
+export const BankButton = styled.button<{ isSelected: boolean }>`
+  width: 6.875rem; // 110px
+  height: 5.75rem; // 92px
+  cursor: pointer;
+  border-radius: 12px;
+  border: 2px solid ${({ isSelected }) => isSelected ? '#FF802E' : 'none'};
+  background-color: #FAF6F3;
+  padding: 1rem; // 20px
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+export const BankName = styled.p`
+  font-size: 0.9rem; // 14px
+  font-weight: 400;
+`;
---
+++ src/pages/createBill/addAccountStep/components/BankNameDrawer/index.tsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import {
+  DrawerBackdrop,
+  DrawerContent,
+  DrawerRoot,
+  DrawerTrigger,
+} from '@/common/components/Drawer/drawer';
+import * as S from './index.style';
+import { Flex } from '@chakra-ui/react';
+import { BankList } from '../../constants/BankList';
+
+interface BankNameDrawerProps {
+  open: boolean;
+  onClose: () => void;
+  setBankName: (name: string) => void;
+}
+
+function BankNameDrawer({ open, onClose, setBankName }: BankNameDrawerProps) {
+  const [name, setName] = useState<string>('');
+
+  const handleSubmitButtonClick = () => {
+    if (!name) return;
+    setBankName(name);
+    onClose();
+  };
+
+  return (
+    <>
+      <DrawerRoot
+        open={open}
+        placement="bottom" // 드로어가 바닥에서 올라오도록 설정
+      >
+        <DrawerBackdrop />
+        <DrawerContent style={{ borderRadius: '12px 12px 0 0' }}>
+          <Flex
+            direction="column"
+            gap={7}
+            justify="space-between"
+            pt={8}
+            px={5}
+            height="70dvh"
+            borderRadius={12}
+          >
+            <S.DrawerHeader>은행 선택</S.DrawerHeader>
+            <S.DrawerBody>
+              {BankList.map((bank) => (
+                <S.BankButton
+                  key={bank}
+                  onClick={() => setName(bank)}
+                  isSelected={name === bank}
+                >
+                  <S.BankName>{bank}</S.BankName>
+                </S.BankButton>
+              ))}
+            </S.DrawerBody>
+            <DrawerTrigger asChild>
+              <S.SubmitButton onClick={handleSubmitButtonClick}>
+                확인
+              </S.SubmitButton>
+            </DrawerTrigger>
+          </Flex>
+        </DrawerContent>
+      </DrawerRoot>
+    </>
+  );
+}
+
+export default BankNameDrawer;
