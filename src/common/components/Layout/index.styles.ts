e125d39688656f65b3aabb6d970c0206bf0bf472
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-14 22:11:44 +0900

    design: Layout 높이 고정
    스크롤이 필요한 경우에만 지정하기 위해서 Layout 높이 고정

--- src/common/components/Layout/index.styles.ts
+++ src/common/components/Layout/index.styles.ts
@@ -4,7 +4,7 @@ export const Wrapper = styled.div`
   overflow-x: hidden;
   max-width: 600px;
   min-width: 320px;
-  height: auto;
+  height: 100dvh;
   width: 100%;
   background-color: white;
   min-height: 100dvh;
