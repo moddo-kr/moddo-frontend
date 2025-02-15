8c26185dc9796352a34513693da2ff79e861a47d
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-16 02:01:40 +0900

    design: 아이콘 색깔을 변경할 수 있도록 수정

--- src/assets/svgs/icon/ArrowRight.tsx
+++ src/assets/svgs/icon/ArrowRight.tsx
@@ -3,12 +3,11 @@ import type { SVGProps } from 'react';
 const SvgArrowRight = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    fill="none"
+    fill="#444950"
     viewBox="0 0 24 24"
     {...props}
   >
     <path
-      fill="#444950"
       fillRule="evenodd"
       d="M8.405 19.735a.8.8 0 0 0 1.13.06l8-7.2a.8.8 0 0 0 0-1.19l-8-7.2a.8.8 0 1 0-1.07 1.19L15.805 12l-7.34 6.605a.8.8 0 0 0-.06 1.13"
       clipRule="evenodd"
