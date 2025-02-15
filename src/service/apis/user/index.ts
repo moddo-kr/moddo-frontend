e222e0870148de0d9bf3ca0a2a37df74bbd998ba
ongheong <pudding4271@gmail.com>
2025-02-15 09:43:58 +0900

    feat: guestLogin api 요청 및 useMustation 메서드 정의

--- src/service/apis/user/index.ts
+++ src/service/apis/user/index.ts
@@ -1,1 +1,33 @@
-/** @Todo user 관련 api 호출 함수 작성 */
+import { useMutation } from '@tanstack/react-query';
+import axiosInstance from '../axios';
+import { ROUTE } from '@/common/constants/route';
+import { useNavigate } from 'react-router';
+
+export interface GetGuestToken {
+  accessToken: string;
+  refreshToken: string;
+  expiredAt: Date;
+  isMember: boolean;
+}
+
+export const getGuestToken = async () => {
+  const response = await axiosInstance.get('/user/guest/token');
+  return response.data;
+};
+
+export const useGetGuestToken = () => {
+  const navigate = useNavigate();
+  return useMutation<GetGuestToken, Error>({
+    mutationFn: () => {
+      return getGuestToken();
+    },
+    onSuccess: (response) => {
+      localStorage.setItem('accessToken', 'Bearer ' + response?.accessToken);
+      navigate(ROUTE.selectGroup);
+    },
+    onError: (error) => {
+      alert('guestToken 발급을 실패했습니다!');
+      console.error('Error creating meeting:', error);
+    }
+  });
+};
