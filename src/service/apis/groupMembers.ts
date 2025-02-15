828d0e0a608d3453a27cba7c19e35145f61fbba5
ongheong <pudding4271@gmail.com>
2025-02-16 04:05:59 +0900

    feat: groupMembers.post API 호출 함수 정의

---
+++ src/service/apis/groupMembers.ts
@@ -0,0 +1,19 @@
+import axiosInstance from './axios';
+
+export interface CreateGroupMembersVariable {
+  name: string;
+};
+
+const groupMembers = {
+  /** body에 {name: string}[] 을 전달해야 하며, 총무는 제외한다. */
+  post: async (groupMemberData: CreateGroupMembersVariable[]) => {
+    const groupToken = localStorage.getItem('groupToken');
+    const response = await axiosInstance.post(`group-members?groupToken=${groupToken}`, groupMemberData, {
+      useMock: true,
+    });
+    console.log(response);
+    return response.data.members;
+  },
+};
+
+export default groupMembers;
