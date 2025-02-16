c85e683a12e2348a3ae8995db7b84b89894f7155
ongheong <pudding4271@gmail.com>
2025-02-16 16:45:42 +0900

    fix: useGetGuestToken에서 return response 추가

--- src/common/queries/auth/useGetGuestToken.ts
+++ src/common/queries/auth/useGetGuestToken.ts
@@ -12,9 +12,9 @@ export const useGetGuestToken = () => {
       if (response?.accessToken) {
         localStorage.setItem('accessToken', `Bearer ${response?.accessToken}`);
         navigate(ROUTE.selectGroup);
-      } else {
-        console.error('Access token not found');
-      }
+        return response;
+      } 
+      throw new Error('Access Token not found');
     },
     enabled: false, // refetch가 호출될 때만 실행되도록 설정
   });
