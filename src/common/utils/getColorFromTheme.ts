4148e9b013de1261737cab2c3746fc47944795ad
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-16 20:56:54 +0900

    style: 주석 오타 수정

--- src/common/utils/getColorFromTheme.ts
+++ src/common/utils/getColorFromTheme.ts
@@ -1,7 +1,7 @@
 import { ColorKey, ColorType } from '@/styles/theme.type';
 
 /**
- * dot notion으로 구분된 키를 이용하여 테마 Color에서 색상을 가져옵니다.
+ * dot notation으로 표현된 키를 이용하여 테마 Color에서 색상을 가져옵니다.
  * @param themeColor 테마 Color 객체
  * @param key dot notation으로 구분된 색상 키
  * @returns 색상 문자열 또는 undefined
