ef5b0fabc375fac4868e706463c1e757c13604dc
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-16 22:34:58 +0900

    refactor: TextVariant 재사용을 위한 css helper 정의
    - TextVariant css helper 정의
    - css helper를 사용할 수 있도록 Text 스타일링 방식을 템플릿 리터럴로 변경

--- src/common/components/Text/index.styles.tsx
+++ src/common/components/Text/index.styles.tsx
@@ -1,19 +1,22 @@
 import getColorFromTheme from '@/common/utils/getColorFromTheme';
 import { ColorKey, TypographyKey } from '@/styles/theme.type';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+export const TextVariant = (variant: TypographyKey) => css`
+  font-family: ${({ theme }) => theme.typography.fontFamily};
+  font-size: ${({ theme }) => theme.typography.fontSize[variant]};
+  font-weight: ${({ theme }) => theme.typography.fontWeight[variant]};
+  line-height: ${({ theme }) => theme.typography.lineHeight[variant]};
+  letter-spacing: ${({ theme }) =>
+    theme.typography.letterSpacing.typoFontSpacing0};
+`;
 
 interface StyledTextProps {
   variant: TypographyKey;
   color: ColorKey;
 }
 
-export const Text = styled.span<StyledTextProps>(
-  ({ theme, variant, color }) => ({
-    fontFamily: theme.typography.fontFamily,
-    fontSize: theme.typography.fontSize[variant],
-    fontWeight: theme.typography.fontWeight[variant],
-    lineHeight: theme.typography.lineHeight[variant],
-    letterSpacing: theme.typography.letterSpacing.typoFontSpacing0,
-    color: getColorFromTheme(theme.color, color) ?? 'inherit',
-  })
-);
+export const Text = styled.span<StyledTextProps>`
+  ${({ variant }) => TextVariant(variant)};
+  color: ${({ theme, color }) => getColorFromTheme(theme.color, color)};
+`;
