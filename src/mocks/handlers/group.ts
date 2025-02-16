894d587fd17ae3ca5f5d2627bcf3af119323a9f1
ongheong <pudding4271@gmail.com>
2025-02-16 23:09:17 +0900

    feat: groupHandlers에 UpdateAccount mocking 정의

--- src/mocks/handlers/group.ts
+++ src/mocks/handlers/group.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse, passthrough } from 'msw';
 import getIsMocked from '@/mocks/utils/getIsMocked';
+import { AccountVariable } from '@/service/apis/group';
 
 const dummyGroups = [
   {
@@ -49,6 +50,36 @@ const groupHandlers = [
       groupToken: import.meta.env.VITE_MOCK_GROUP_TOKEN,
     });
   }),
+
+  http.put<object, AccountVariable>(
+    '/api/v1/group/account',
+    async ({ request }) => {
+      if (!getIsMocked(request)) return passthrough();
+
+      const url = new URL(request.url);
+      const groupToken = url.searchParams.get('groupToken');
+
+      if (!groupToken) {
+        return HttpResponse.json({
+          error: 'groupToken is required',
+          status: 401,
+        });
+      }
+
+      const body = await request.json();
+      const { bank, accountNumber } = body;
+      return HttpResponse.json({
+        id: 1,
+        writer: 1,
+        createAt: new Date().toISOString(),
+        expiredAt: new Date(
+          new Date().setMonth(new Date().getMonth() + 1)
+        ).toISOString(),
+        bank: bank,
+        accountNumber: accountNumber,
+      });
+    }
+  ),
 ];
 
 export default groupHandlers;
