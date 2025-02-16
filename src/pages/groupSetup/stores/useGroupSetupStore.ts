a1998e59ed279dbc6a812881910809951e47a746
ongheong <pudding4271@gmail.com>
2025-02-16 21:43:17 +0900

    fix: 불필요한 GroupMember 전역상태변수 store 삭제

--- src/pages/groupSetup/stores/useGroupSetupStore.ts
+++ src/pages/groupSetup/stores/useGroupSetupStore.ts
@@ -1,20 +1,15 @@
-import { Member } from '@/common/types/member.type';
 import { create } from 'zustand';
 
 interface GroupSetupState {
   groupName: string;
   password: string;
-  members: Member[];
   setGroupName: (groupName: string) => void;
   setPassword: (password: string) => void;
-  setMembers: (members: Member[]) => void;
 }
 
 export const useGroupSetupStore = create<GroupSetupState>((set) => ({
   groupName: '',
   password: '',
-  members: [],
   setGroupName: (groupName) => set({ groupName }),
   setPassword: (password) => set({ password }),
-  setMembers: (members) => set({ members }),
 }));
