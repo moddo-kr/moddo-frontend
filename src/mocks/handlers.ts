76f4274b089504756e73a43051ea0448e2f14ac9
ongheong <pudding4271@gmail.com>
2025-02-16 21:45:49 +0900

    chore: handlers.ts에 groupMemberHandler 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,11 @@
 import authHandlers from './handlers/auth';
 import expenseHandlers from './handlers/expense';
 import groupHandlers from './handlers/group';
+import groupMemberHandlers from './handlers/groupMemberHandlers';
 
-export const handlers = [...expenseHandlers, ...authHandlers, ...groupHandlers];
+export const handlers = [
+  ...expenseHandlers,
+  ...authHandlers,
+  ...groupHandlers,
+  ...groupMemberHandlers,
+];
