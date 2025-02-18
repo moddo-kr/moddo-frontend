f765fbfba87e7167085d889b8ce08f9c600ffa86
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-18 20:14:15 +0900

    design: 지출 내역 상세 페이지 기본 레이아웃 구성

--- src/pages/billDetail/index.tsx
+++ src/pages/billDetail/index.tsx
@@ -1,9 +1,12 @@
 import { useLoaderData } from 'react-router';
 import { useTheme } from 'styled-components';
 import { ArrowLeft } from '@/assets/svgs/icon';
+import Button from '@/common/components/Button';
 import Header from '@/common/components/Header';
 import Text from '@/common/components/Text';
 import { GroupTokenUrlLoaderData } from '@/common/types/group.type';
+import { BottomButtonContainer } from '@/styles/bottomButton.styles';
+import * as S from './index.styles';
 
 function BillDetail() {
   const { unit } = useTheme();
@@ -12,22 +15,26 @@ function BillDetail() {
   // TODO : 필요하다면 useQuery의 initialData에 groupData를 넣어서 사용
 
   return (
-    // <>
-    <Header
-      type="TitleCenter"
-      leftButtonContent={
-        <>
-          <ArrowLeft width={unit[24]} />
-          <Text variant="body1R">{groupData.groupName}</Text>
-        </>
-      }
-      rightButtonContent={
-        <Text variant="body1R" color="semantic.text.subtle">
-          관리
-        </Text>
-      }
-    />
-    // </>
+    <>
+      <Header
+        type="TitleCenter"
+        leftButtonContent={
+          <>
+            <ArrowLeft width={unit[24]} />
+            <Text variant="body1R">{groupData.groupName}</Text>
+          </>
+        }
+        rightButtonContent={
+          <Text variant="body1R" color="semantic.text.subtle">
+            관리
+          </Text>
+        }
+      />
+      <S.Content>내용</S.Content>
+      <BottomButtonContainer>
+        <Button>링크 공유하기</Button>
+      </BottomButtonContainer>
+    </>
   );
 }
 
