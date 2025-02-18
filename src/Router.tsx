f28a55359fdae7936fce2e4706919e4269dce5d7
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-19 01:27:09 +0900

    fix: msw worker를 loader에서도 사용할 수 있게 수정
    - 특정 브라우저에서는 최상위에서 await을 사용하지 못하는 문제가 있음
    - enableMocking 실행 이후에 라우트를 생성할 수 있도록 AppRouter 내부에서 createBrowserRouter를 생성하도록 함

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import { checkAuth, groupTokenUrlLoader } from '@/common/loaders';
 import { ROUTE } from '@/common/constants/route';
-import enableMocking from '@/mocks/utils/enableMocking';
 import SelectGroup from '@/pages/selectGroup';
 import GroupNameSetup from '@/pages/groupSetup/groupNameSetup';
 import PasswordSetup from '@/pages/groupSetup/passwordSetup';
@@ -12,59 +11,57 @@ import LoginSuccess from '@/pages/auth/loginSuccess';
 import Home from '@/pages/home';
 import BillDetail from '@/pages/billDetail';
 
-await enableMocking();
-
-const router = createBrowserRouter([
-  {
-    path: '*',
-    element: <Login />,
-  },
-  {
-    path: ROUTE.login,
-    element: <Login />,
-  },
-  {
-    path: ROUTE.loginSuccess,
-    element: <LoginSuccess />,
-  },
-  {
-    path: ROUTE.home,
-    element: <Home />,
-    loader: checkAuth,
-  },
-  {
-    path: ROUTE.selectGroup,
-    element: <SelectGroup />,
-    loader: checkAuth,
-  },
-  {
-    path: ROUTE.groupSetupName,
-    element: <GroupNameSetup />,
-    loader: checkAuth,
-  },
-  {
-    path: ROUTE.groupSetupPassword,
-    element: <PasswordSetup />,
-    loader: checkAuth,
-  },
-  {
-    path: ROUTE.groupSetupMember,
-    element: <MemberSetup />,
-    loader: checkAuth,
-  },
-  {
-    path: ROUTE.createBill,
-    element: <CreateBill />,
-    loader: checkAuth,
-  },
-  {
-    path: ROUTE.billDetail,
-    element: <BillDetail />,
-    loader: groupTokenUrlLoader,
-  },
-]);
-
 function AppRouter() {
+  const router = createBrowserRouter([
+    {
+      path: '*',
+      element: <Login />,
+    },
+    {
+      path: ROUTE.login,
+      element: <Login />,
+    },
+    {
+      path: ROUTE.loginSuccess,
+      element: <LoginSuccess />,
+    },
+    {
+      path: ROUTE.home,
+      element: <Home />,
+      loader: checkAuth,
+    },
+    {
+      path: ROUTE.selectGroup,
+      element: <SelectGroup />,
+      loader: checkAuth,
+    },
+    {
+      path: ROUTE.groupSetupName,
+      element: <GroupNameSetup />,
+      loader: checkAuth,
+    },
+    {
+      path: ROUTE.groupSetupPassword,
+      element: <PasswordSetup />,
+      loader: checkAuth,
+    },
+    {
+      path: ROUTE.groupSetupMember,
+      element: <MemberSetup />,
+      loader: checkAuth,
+    },
+    {
+      path: ROUTE.createBill,
+      element: <CreateBill />,
+      loader: checkAuth,
+    },
+    {
+      path: ROUTE.billDetail,
+      element: <BillDetail />,
+      loader: groupTokenUrlLoader,
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import enableMocking from './mocks/utils/enableMocking';
 import App from './App';
 
 const queryClient = new QueryClient();
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-);
+enableMocking().then(() => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  createRoot(document.getElementById('root')!).render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
+});
