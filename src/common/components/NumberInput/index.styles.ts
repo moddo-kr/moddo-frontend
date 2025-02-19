e635863c325c24bf69645d98a1f3b768ff5791cf
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-19 17:22:43 +0900

    design: NumberInput의 크기가 넘치는 문제 해결

--- src/common/components/NumberInput/index.styles.ts
+++ src/common/components/NumberInput/index.styles.ts
@@ -3,15 +3,20 @@ import { TextVariant } from '../Text/index.styles';
 
 export const Container = styled.div`
   display: flex;
+  box-sizing: border-box;
   gap: ${({ theme }) => theme.unit[8]};
   padding: ${({ theme }) => `${theme.unit[10]} 0`};
   border-bottom: 1px solid ${({ theme }) => theme.color.semantic.border.default};
+  flex-grow: 1;
+  min-width: 0; // 자식 요소가 부모 요소의 크기를 넘어가지 않도록 함
 `;
 
 export const NumberInput = styled.input<{ $variant?: 'lg' | 'sm' }>`
   all: unset;
   flex: 1;
   text-align: right;
+  min-width: 0; // 자식 요소가 부모 요소의 크기를 넘어가지 않도록 함
+  flex-shrink: 1;
   ${({ $variant }) =>
     $variant === 'lg' ? TextVariant('title') : TextVariant('body1Sb')};
   color: ${({ $variant, theme }) =>
