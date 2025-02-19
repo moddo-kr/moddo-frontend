d1c71cfa1a776361001343528d473ce6a88fcbda
ongheong <pudding4271@gmail.com>
2025-02-19 23:22:27 +0900

    feat: CurvedProgressBar 컴포넌트 구현

---
+++ src/common/components/CurvedProgressBar/index.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from 'react';
+import {
+  buildStyles,
+  CircularProgressbarWithChildren,
+} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import * as S from './index.style';
+import { useTheme } from 'styled-components';
+import ChangingProgressProvider from './ChangingProgressProvider';
+
+interface CurvedProgressBar {
+  percentage: number;
+  children?: ReactNode;
+}
+
+function CurvedProgressBar({ percentage, children }: CurvedProgressBar) {
+  const theme = useTheme();
+
+  return (
+    <S.Wrapper>
+      <ChangingProgressProvider values={[0, percentage]}>
+        {(percentage) => (
+          <CircularProgressbarWithChildren
+            value={percentage}
+            circleRatio={0.5}
+            strokeWidth={11}
+            styles={buildStyles({
+              rotation: 1 / 2 + 1 / 4,
+              strokeLinecap: 'round',
+              trailColor: `${theme.color.semantic.secondary.strong}`,
+              pathColor: `${theme.color.semantic.orange.default}`,
+            })}
+          >
+            {children}
+          </CircularProgressbarWithChildren>
+        )}
+      </ChangingProgressProvider>
+    </S.Wrapper>
+  );
+}
+
+export default CurvedProgressBar;
