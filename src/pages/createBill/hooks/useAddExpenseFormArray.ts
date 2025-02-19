880c5acd73a9023a776b919b68504f63d3f91d7d
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-20 01:41:50 +0900

    feat: 지출 폼 내 그룹 토큰 로직 추가

--- src/pages/createBill/hooks/useAddExpenseFormArray.ts
+++ src/pages/createBill/hooks/useAddExpenseFormArray.ts
@@ -1,5 +1,6 @@
 import { useMemo, useState } from 'react';
 import { useFieldArray, useForm } from 'react-hook-form';
+import { useLoaderData } from 'react-router';
 import { format } from 'date-fns';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Group } from '@/common/types/group.type';
@@ -20,13 +21,13 @@ const defaultValues: SingleExpenseForm = {
  * 지출 폼을 위한 커스텀 훅
  */
 const useAddExpenseFormArray = (initialExpense?: SingleExpenseForm) => {
+  const { groupToken } = useLoaderData();
   const [groupInfo, setGroupInfo] = useState<Group | null>(null);
   const formMethods = useForm({
     resolver: zodResolver(ExpenseFormSchema),
     mode: 'onChange', // 폼들의 필수 입력값이 모두 입력되었을 때 '다음' 버튼을 활성화시키기 위함
     defaultValues: async () => {
-      // TODO : groupToken을 받아오는 로직 추가
-      const groupData = await group.get('groupToken');
+      const groupData = await group.get(groupToken);
       setGroupInfo(groupData);
       // 기본 데이터가 있는 경우 (ex. 수정)
       if (initialExpense) {
