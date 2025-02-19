447d080d42942a48ed14580e27aed1d150595936
ongheong <pudding4271@gmail.com>
2025-02-19 23:28:29 +0900

    feat: billDetail 페이지 Header에 CurvedProgressBar 및 스타일 추가

--- src/pages/billDetail/index.tsx
+++ src/pages/billDetail/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useLoaderData } from 'react-router';
 import { useTheme } from 'styled-components';
-import { ArrowLeft } from '@/assets/svgs/icon';
+import { ArrowLeft, Crown } from '@/assets/svgs/icon';
 import Button from '@/common/components/Button';
 import Header from '@/common/components/Header';
 import { TabsList, Tab } from '@/common/components/Tabs';
@@ -9,13 +9,25 @@ import Text from '@/common/components/Text';
 import { BottomButtonContainer } from '@/styles/bottomButton.styles';
 import * as S from './index.styles';
 import ExpenseTimeline from './components/ExpenseTimeline';
+import CurvedProgressBar from '@/common/components/CurvedProgressBar';
+import ModdoFace from '@/assets/pngs/moddoFace.png';
+import { DollarCircle } from '@/assets/svgs/icon';
+
+const MEMBER_TOTAL: number = 6;
+const MEMBER_DONE: number = 3;
+const TOTAL_MONEY: number = 120000;
 
 function BillDetail() {
   const { unit } = useTheme();
   const [activeTab, setActiveTab] = useState('member');
   const { groupToken, groupData } = useLoaderData();
   // TODO : 필요하다면 useQuery의 initialData에 groupData를 넣어서 사용
-
+  const theme = useTheme();
+  const percentage = (MEMBER_DONE / MEMBER_TOTAL) * 100;
+  const crownColor =
+    MEMBER_DONE == MEMBER_TOTAL
+      ? theme.color.primitive.base.white
+      : theme.color.semantic.secondary.heavy;
   return (
     <>
       <Header
@@ -33,7 +45,25 @@ function BillDetail() {
         }
       />
       <S.Content>
-        {/* TODO : 정산 현황 */}
+        <CurvedProgressBar percentage={percentage}>
+          <S.ModdoImage src={ModdoFace}></S.ModdoImage>
+          <S.ExpenseChip>
+            <DollarCircle
+              width="32"
+              style={{ paddingRight: `${theme.unit[8]}` }}
+            />
+            <Text as="p" variant="body1Sb" color="semantic.orange.default">
+              {MEMBER_DONE}
+            </Text>
+            {`/${MEMBER_TOTAL} 정산 완료`}
+          </S.ExpenseChip>
+          <Crown
+            width={theme.unit[24]}
+            fill={crownColor}
+            style={{ position: 'absolute', top: '44.5%', right: '1.5%' }}
+          />
+          <S.TotalMoney>{TOTAL_MONEY.toLocaleString('ko-KR')}원</S.TotalMoney>
+        </CurvedProgressBar>
         <S.TabListContainer>
           <TabsList activeTab={activeTab} setActiveTab={setActiveTab}>
             <Tab label="참여자별 정산" value="member" />
