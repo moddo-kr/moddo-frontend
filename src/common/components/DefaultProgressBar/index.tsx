3249d30e3296b32bf9ef54760fb3e29c58619160
ongheong <pudding4271@gmail.com>
2025-02-20 01:52:44 +0900

    fix: DefaultProgressBar 스토리 정의에 따른 DefaultProgressBar Props 객체로 분리

--- src/common/components/DefaultProgressBar/index.tsx
+++ src/common/components/DefaultProgressBar/index.tsx
@@ -1,6 +1,10 @@
 import * as S from './index.style';
 
-function DefaultProgressBar(percentage: number) {
+interface DefaultProgressBar {
+  percentage: number;
+}
+
+function DefaultProgressBar({ percentage }: DefaultProgressBar) {
   return (
     <S.Trail>
       <S.Path percentage={percentage} />
