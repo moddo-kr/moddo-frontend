e9f2ac6022c9c6de4e1315b1b6dae57ddef4d03a
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-20 02:48:43 +0900

    fix: 오류 제거를 위한 주석 해제

--- src/service/apis/groupMembers.ts
+++ src/service/apis/groupMembers.ts
@@ -7,18 +7,18 @@ export interface CreateGroupMembersVariable {
 
 const groupMembers = {
   // NOTE : 사용하지 않는 API인지 확인 필요합니다.
-  // /** body에 {name: string}[] 을 전달해야 하며, 총무는 제외한다. */
-  // post: async (groupMemberData: CreateGroupMembersVariable[]) => {
-  //   const groupToken = localStorage.getItem('groupToken');
-  //   const response = await axiosInstance.post(
-  //     `/group-members?groupToken=${groupToken}`,
-  //     { members: groupMemberData },
-  //     {
-  //       useMock: true,
-  //     }
-  //   );
-  //   return response.data.members;
-  // },
+  /** body에 {name: string}[] 을 전달해야 하며, 총무는 제외한다. */
+  post: async (groupMemberData: CreateGroupMembersVariable[]) => {
+    const groupToken = localStorage.getItem('groupToken');
+    const response = await axiosInstance.post(
+      `/group-members?groupToken=${groupToken}`,
+      { members: groupMemberData },
+      {
+        useMock: true,
+      }
+    );
+    return response.data.members;
+  },
   // PUT addGroupMember
   put: async ({
     groupMemberData,
