c9bc409d297ee4553d5b9c09669b25c2f9134850
ongheong <pudding4271@gmail.com>
2025-02-19 13:34:57 +0900

    chore: 기존 BankList 삭제 및 BankList 배열을 리턴하는 유틸 함수 구현

---
+++ src/pages/createBill/addAccountStep/utils/BankList.ts
@@ -0,0 +1,16 @@
+/**
+ * 파일 경로에서 bankName과 URL을 매핑하여 배열 리턴
+ * 파일 이름에서 bankName을 추출하고, URL을 매핑
+ * bankName=경남.png -> 경남
+ **/
+export const getBankImagesAndUrl = () => {
+  const files = import.meta.glob('/public/pngs/banks/bankName=*.png');
+
+  const bankNames = Object.keys(files).map((filePath) => {
+    const bankName = filePath.split('=')[1].split('.')[0];
+    const url = `/public/pngs/banks/${filePath.split('/').pop()}`;
+    return { bankName, url };
+  });
+
+  return bankNames;
+};
