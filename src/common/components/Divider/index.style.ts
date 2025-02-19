5d33866c8342f6e532b641e7da8189ead8cec3eb
ongheong <pudding4271@gmail.com>
2025-02-20 03:04:14 +0900

    chore: divider에 min-width 추가

--- src/common/components/Divider/index.style.ts
+++ src/common/components/Divider/index.style.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Divider = styled.div<{ height: number }>`
   width: 100dvw;
   max-width: 37.5rem;
+  min-width: 20rem;
   background: #e8e8e7;
   height: ${({ theme, height }) => theme.unit[height]};
 `;
