9cdf36e452d2b259d40ad307bf219986c09fad0d
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-20 11:26:26 +0900

    feat: QR 페이지 레이아웃 구성

---
+++ src/pages/createBill/qrStep/index.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const QrField = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  flex: 1;
+  overflow-y: auto;
+`;
--- src/pages/createBill/qrStep/index.tsx
+++ src/pages/createBill/qrStep/index.tsx
@@ -1,7 +1,11 @@
+import { ArrowLeft, Reset } from '@/assets/svgs/icon';
 import { BaseFunnelStepComponentProps } from '@/common/types/useFunnel.type';
 import Header from '@/common/components/Header';
-import { ArrowLeft, Reset } from '@/assets/svgs/icon';
+import Button from '@/common/components/Button';
+import DescriptionField from '@/common/components/DescriptionField';
+import { BottomButtonContainer } from '@/styles/bottomButton.styles';
 import { BillContext } from '../types/billContext.type';
+import * as S from './index.styles';
 
 interface QrStepProps extends BaseFunnelStepComponentProps<BillContext> {}
 
@@ -10,13 +14,20 @@ function QrStep({ moveToPreviousStep }: QrStepProps) {
     <>
       <Header
         type="TitleCenter"
+        title="QR코드"
         leftButtonContent={<ArrowLeft width="1.5rem" />}
         leftButtonOnClick={() => {
           moveToPreviousStep?.();
         }}
         rightButtonContent={<Reset width="1.5rem" />}
       />
-      <div>QR</div>
+      <DescriptionField
+        title={`QR코드를 공유하면\n바로 정산에 참여할 수 있어요!`}
+      />
+      <S.QrField>QR field</S.QrField>
+      <BottomButtonContainer>
+        <Button>다운로드</Button>
+      </BottomButtonContainer>
     </>
   );
 }
