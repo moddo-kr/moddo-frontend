a917ef04b267623e807f97c7b68675f0dbb913ff
ongheong <pudding4271@gmail.com>
2025-02-21 02:12:05 +0900

    feat: 정산 상황을 나타내는 StatusType 정의

---
+++ src/pages/billDetail/components/ExpenseTimeHeader/index.type.ts
@@ -0,0 +1,24 @@
+import ModdoFace from '@/assets/pngs/moddoFace.png';
+import CryingModdoFace from '@/assets/pngs/CryingModdoFace.png';
+
+export type StatusType = 'pending' | 'success' | 'failure';
+
+export type StatusInfo = {
+  message: string;
+  image: string;
+}
+
+export const StatusContent : Record<StatusType, StatusInfo> = {
+  pending: {
+    message: '마감 전에 정산하면, 귀여운 캐릭터가 뿅!',
+    image: ModdoFace,
+  },
+  success: {
+    message: '정산, 완벽하게 클리어!',
+    image: ModdoFace,
+  },
+  failure: {
+    message: '정산 마감 시간이 초과되었어요',
+    image: CryingModdoFace,
+  },
+}
