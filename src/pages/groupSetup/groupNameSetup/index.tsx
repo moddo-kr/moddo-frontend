ca0c4c382a8d49095e2dfffa126b261ea336ccbe
yoouyeon <jyeon.yoon59@gmail.com>
2025-02-21 21:00:55 +0900

    design: 모임 입력 페이지 하이파이 디자인 적용

--- src/pages/groupSetup/groupNameSetup/index.tsx
+++ src/pages/groupSetup/groupNameSetup/index.tsx
@@ -1,11 +1,17 @@
-import { Button, Flex, Input } from '@chakra-ui/react';
+import { useTheme } from 'styled-components';
 import { useNavigate } from 'react-router';
 import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { ArrowLeft } from '@/assets/svgs/icon';
+import Text from '@/common/components/Text';
 import Header from '@/common/components/Header';
 import { useGroupSetupStore } from '@/pages/groupSetup/stores/useGroupSetupStore';
 import { ROUTE } from '@/common/constants/route';
+import DescriptionField from '@/common/components/DescriptionField';
+import { BottomButtonContainer } from '@/styles/bottomButton.styles';
+import Button from '@/common/components/Button';
+import Input from '@/common/components/Input';
 import * as S from '../index.styles';
 
 const groupNameSchema = z.object({
@@ -15,7 +21,9 @@ const groupNameSchema = z.object({
 });
 
 function GroupNameSetup() {
+  const { unit } = useTheme();
   const { groupName, setGroupName } = useGroupSetupStore();
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -26,8 +34,6 @@ function GroupNameSetup() {
     mode: 'onChange',
   });
 
-  const navigate = useNavigate();
-
   const onNext = (data: { groupName: string }) => {
     setGroupName(data.groupName);
     navigate(ROUTE.groupSetupPassword);
@@ -36,48 +42,32 @@ function GroupNameSetup() {
   return (
     <>
       <Header
-        title=""
-        showIcon={false}
-        type="TitleLeft"
-        handleBackButtonClick={() => navigate(-1)}
+        type="TitleCenter"
+        leftButtonContent={
+          <>
+            <ArrowLeft width={unit[24]} />
+            <Text>뒤로가기</Text>
+          </>
+        }
+        leftButtonOnClick={() => navigate(-1)}
       />
-      <Flex
-        direction="column"
-        justify="space-between"
-        mx="5"
-        height="100%"
-        mt="10px"
-        mb="32px"
-        flexGrow={1}
-      >
-        <Flex direction="column">
-          <S.TitleText>
-            모임 이름을
-            <br />
-            입력해주세요.
-          </S.TitleText>
-          <Input
-            borderRadius={12}
-            placeholder="DND 7조 첫모임"
-            fontSize={16}
-            py={3}
-            height={12}
-            {...register('groupName')}
-            mb={4}
-          />
-          {errors.groupName && (
-            <S.ErrorText>* {errors.groupName?.message?.toString()}</S.ErrorText>
-          )}
-        </Flex>
-        <Button
-          height={12}
-          borderRadius={12}
-          onClick={handleSubmit(onNext)}
-          disabled={!isValid}
-        >
+      <DescriptionField
+        title={`생성할 모임의\n이름을 입력해주세요.`}
+        sub="모임 이름은 수정이 불가능해요. "
+      />
+      <S.PageContentWrapper>
+        <Input placeholder="모또 미팅" {...register('groupName')} />
+        {errors.groupName ? (
+          <Text as="p" variant="caption" color="semantic.state.danger">
+            {errors.groupName?.message?.toString()}
+          </Text>
+        ) : null}
+      </S.PageContentWrapper>
+      <BottomButtonContainer>
+        <Button onClick={handleSubmit(onNext)} disabled={!isValid}>
           다음
         </Button>
-      </Flex>
+      </BottomButtonContainer>
     </>
   );
 }
--- src/pages/groupSetup/index.styles.ts
+++ src/pages/groupSetup/index.styles.ts
@@ -1,20 +1,8 @@
 import styled from 'styled-components';
 
-export const ErrorText = styled.span`
-  margin: 0;
-  color: red;
-  font-size: 0.75rem;
-  font-weight: 400;
-`;
-
-export const TitleText = styled.h1`
-  all: unset;
+export const PageContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
-  font-weight: 600;
-  margin-bottom: 2rem; // 32px
-  white-space: pre-wrap;
-  font-size: 1.25rem; // 20px
-  line-height: 1.75rem; // 28px
+  padding: ${({ theme }) => `${theme.unit[28]} ${theme.unit[20]}`};
+  flex: 1;
 `;
