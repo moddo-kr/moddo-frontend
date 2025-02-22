b65ec5bed94ad84c77b5a0c948548f3786a449bc
ongheong <pudding4271@gmail.com>
2025-02-22 11:36:49 +0900

    chore: DefaultProgressBar 수정

--- src/common/components/DefaultProgressBar/index.style.ts
+++ src/common/components/DefaultProgressBar/index.style.ts
@@ -4,10 +4,12 @@ export const Trail = styled.div`
   width: 100%;
   height: ${({ theme }) => theme.unit[8]};
   background: ${({ theme }) => theme.color.semantic.background.normal.default};
+  border-radius: ${({ theme }) => theme.radius.circle};
 `;
 
 export const Path = styled.div<{ percentage: number }>`
   width: ${({ percentage }) => `${percentage}%`};
   height: 100%;
   background: ${({ theme }) => theme.color.semantic.orange.default};
+  border-radius: ${({ theme }) => theme.radius.circle};
 `;
