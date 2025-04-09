32e61a79ebe3ac198a7b3f25e3303ea92abadc08
yoouyeon <jyeon.yoon59@gmail.com>
2025-04-09 21:58:46 +0900

    feat: 기본 에러 페이지 추가

---
+++ src/pages/error/index.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router';
+import errorHam from '@/assets/pngs/error-ham.png';
+import Text from '@/common/components/Text';
+import Button from '@/common/components/Button';
+import * as S from './index.style';
+
+function ErrorPage() {
+  return (
+    <S.Flex>
+      <S.ErrorHamster src={errorHam} alt="error hamster" />
+      <S.DescriptionContainer>
+        <Text variant="heading2" color="semantic.text.default">
+          잠시 문제가 발생했어요
+        </Text>
+        <S.SubText variant="body1R" color="semantic.text.subtle">
+          현재 서버 연결에 문제가 있어요.
+          <br />
+          잠시 후 다시 시도해 주세요.
+        </S.SubText>
+      </S.DescriptionContainer>
+      <Button>
+        <Link to="/home">홈으로 돌아가기</Link>
+      </Button>
+    </S.Flex>
+  );
+}
+
+export default ErrorPage;
