d97910e05a7e7793a34de93b39c06622be18a40b
yoouyeon <jyeon.yoon59@gmail.com>
2025-04-14 20:50:09 +0900

    refactor: useApiError에 에러 바운더리를 쓰지 않는 에러를 거르기 위한 props 추가

--- src/common/hooks/useApiError.ts
+++ src/common/hooks/useApiError.ts
@@ -1,11 +1,15 @@
 import { useCallback, useMemo } from 'react';
 import { isAxiosError } from 'axios';
 
-type HandlerType = {
-  [key in number | 'default']: () => void;
+type DefaultHandlerType = {
+  [key: number]: () => void;
+} & {
+  default: () => void;
 };
 
-const defaultHandlers: HandlerType = {
+type HandlerType = Partial<DefaultHandlerType>;
+
+const defaultHandlers: DefaultHandlerType = {
   // 기본 설정의 throwOnError 옵션을 true로 설정했으므로 기본적으로 useApiError의 핸들러에서는 아무것도 하지 않습니다.
   default: () => {},
 };
@@ -13,17 +17,17 @@ const defaultHandlers: HandlerType = {
 /**
  * API 에러를 처리하는 커스텀 훅입니다.
  * @param customHandlers - 훅을 사용하는 곳에서 정의한 에러 핸들러
- * @param boundaryErrors - ErrorBoundary를 사용할 에러 목록
+ * @param nonBoundaryErrors - ErrorBoundary를 사용하지 않는 에러코드
  * @description
  * 핸들러의 적용 순위는 customHandlers -> defaultHandlers입니다.
  * customHandlers에서 정의한 핸들러가 없을 경우 defaultHandlers의 핸들러가 호출됩니다.
  */
-const useApiError = ({
+const useApiError = <TError = Error>({
   customHandlers,
-  boundaryErrors,
+  nonBoundaryErrors,
 }: {
   customHandlers?: HandlerType;
-  boundaryErrors?: number[];
+  nonBoundaryErrors?: number[];
 }) => {
   const handlers = useMemo(
     () => ({ ...defaultHandlers, ...customHandlers }),
@@ -32,7 +36,7 @@ const useApiError = ({
 
   // customErrorHandler를 우선 처리하고, defaultHandler를 처리합니다.
   const handleError = useCallback(
-    (error: Error) => {
+    (error: TError) => {
       if (isAxiosError(error)) {
         const status = error.response?.status;
         if (status) {
@@ -48,18 +52,20 @@ const useApiError = ({
     [handlers]
   );
 
-  // boundaryErrors에 정의된 에러코드를 받은 경우 true를 반환합니다.
+  // nonBoundaryErrors에 포함된 에러코드인지 확인합니다.
+  // 해당 함수가 true를 반환하면 ErrorBoundary에서 에러를 처리합니다.
+  // 기본값은 true입니다.
   const isBoundaryError = useCallback(
-    (error: Error) => {
+    (error: TError) => {
       if (isAxiosError(error)) {
         const status = error.response?.status;
         if (status) {
-          return boundaryErrors?.includes(status) || false;
+          return !nonBoundaryErrors?.includes(status);
         }
       }
-      return false;
+      return true;
     },
-    [boundaryErrors]
+    [nonBoundaryErrors]
   );
 
   return { handleError, isBoundaryError };
