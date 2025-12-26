aaa6a570b3250c0633a36ada9381264297a65cdd
yoouyeon <jyeon.yoon59@gmail.com>
2025-12-26 22:19:00 +0900

    chore: Storybook preview에서 Chromatic 스냅샷 기본 비활성화

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -7,6 +7,7 @@ import GlobalStyles from '../src/shared/styles/globalStyles';
 
 const preview: Preview = {
   parameters: {
+    chromatic: { disableSnapshot: true }, // 기본적으로 Chromatic 스냅샷 비활성화 (필요한 스토리에서만 활성화한다)
     controls: {
       matchers: {
         color: /(background|color)$/i,
