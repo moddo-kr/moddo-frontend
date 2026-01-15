275ea47cd04d74a438888b6bd84b164b76432b7b
yoouyeon <jyeon.yoon59@gmail.com>
2026-01-15 17:03:40 +0900

    design: Loading component 추가

---
+++ src/shared/ui/Loading/index.tsx
@@ -0,0 +1,32 @@
+import { useTheme } from 'styled-components';
+import Lottie from 'lottie-react';
+import Flex from '@/shared/ui/Flex';
+import Text from '@/shared/ui/Text';
+import RawLoadingDdoddo from '@/shared/assets/lotties/loading-ddoddo.json';
+
+function Loading() {
+  const theme = useTheme();
+
+  return (
+    <Flex
+      backgroundColor={theme.color.semantic.background.normal.default}
+      height="100%"
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      gap={4}
+    >
+      <Lottie animationData={RawLoadingDdoddo} />
+      <Flex direction="column" alignItems="center" gap={4}>
+        <Text variant="heading2" color="semantic.text.strong">
+          LOADING...
+        </Text>
+        <Text variant="body1R" color="semantic.text.subtle">
+          잠시만 기다려주세요
+        </Text>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default Loading;
