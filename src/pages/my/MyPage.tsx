e2c78fd68574829bfb5a1998ea5f1575780a15de
yoouyeon <jyeon.yoon59@gmail.com>
2026-02-18 22:31:13 +0900

    feat: 마이페이지에 프로필 컴포넌트 추가

--- src/pages/my/MyPage.tsx
+++ src/pages/my/MyPage.tsx
@@ -1,5 +1,27 @@
+import { useNavigate } from 'react-router';
+import { useTheme } from 'styled-components';
+import { Menu } from '@/shared/assets/svgs/icon';
+import Header from '@/shared/ui/Header';
+import { ROUTE } from '@/shared/config/route';
+import { MyProfile } from '@/entities/auth/ui';
+
 function MyPage() {
-  return <div>마이페이지</div>;
+  const navigate = useNavigate();
+  const { color } = useTheme();
+
+  return (
+    <>
+      {/* TODO: 디자인 시스템 정리 + 헤더 컴포넌트 정비 후에 다시 스타일링이 필요합니다. */}
+      <Header
+        type="TitleCenter"
+        title="마이페이지"
+        rightButtonContent={<Menu width={24} />}
+        rightButtonOnClick={() => navigate(ROUTE.myEdit)}
+        bgColor={color.semantic.background.normal.alternative}
+      />
+      <MyProfile />
+    </>
+  );
 }
 
 export default MyPage;
