68b4f24e67232775d2261c8fe774341f81f1eb4a
yoouyeon <jyeon.yoon59@gmail.com>
2026-02-21 23:06:18 +0900

    fix: 의도치 않은 버튼 기능을 막기 위한 조치 추가
    https://github.com/moddo-kr/moddo-frontend/pull/26#discussion_r2836208582

--- src/pages/my-edit/ui/TermsLink/index.tsx
+++ src/pages/my-edit/ui/TermsLink/index.tsx
@@ -2,7 +2,11 @@ import * as S from './index.styles';
 
 function TermsLink() {
   // TODO: 이용약관 페이지로 이동하도록 수정
-  return <S.Link href="">이용약관</S.Link>;
+  return (
+    <S.Link href="#" onClick={(e) => e.preventDefault()}>
+      이용약관
+    </S.Link>
+  );
 }
 
 export default TermsLink;
