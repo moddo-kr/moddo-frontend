d1e1a3fc85b18429584d99f987c179e1a4762ba0
yoouyeon <jyeon.yoon59@gmail.com>
2026-02-21 22:35:05 +0900

    feat: 로그아웃과 회원탈퇴 api 함수와 훅 추가

---
+++ src/features/auth/api/useLogout.ts
@@ -0,0 +1,17 @@
+import { useNavigate } from 'react-router';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { ROUTE } from '@/shared/config/route';
+import { logout } from './authApi';
+
+export const useLogout = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      queryClient.clear();
+      navigate(ROUTE.login);
+    },
+  });
+};
