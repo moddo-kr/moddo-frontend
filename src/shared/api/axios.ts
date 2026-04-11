d991b7685907bdb9185b227b8f4a893dc74703e5
yoouyeon <jyeon.yoon59@gmail.com>
2026-04-11 20:38:44 +0900

    refactor: Supabase 관련 코드 제거 및 baseURL 정리

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
   // 환경변수에서 서버 URL을 가져오고, 기본값으로 빈 문자열을 사용하도록 설정
   // 의도적으로 상대경로를 사용해야 하는 경우(예: 스토리북)를 위해서 빈 문자열도 사용할 수 있도록 함
   baseURL: import.meta.env.VITE_SERVER_URL
-    ? `${import.meta.env.VITE_SERVER_URL}/functions/v1`
+    ? `${import.meta.env.VITE_SERVER_URL}/api/v1`
     : '',
   withCredentials: true,
   headers: {
@@ -25,13 +25,6 @@ axiosInstance.interceptors.request.use(
         'X-Mock-Request': 'true',
       });
     }
-    // SUPABASE용 apikey 헤더 추가 (필요 시)
-    else if (newConfig.url?.split('?')[0].endsWith('user/guest/token')) {
-      newConfig.headers = AxiosHeaders.from({
-        ...newConfig.headers,
-        apikey: import.meta.env.VITE_SUPABASE_PUBLIC_KEY,
-      });
-    }
     return newConfig;
   },
   (error) => {
@@ -40,7 +33,9 @@ axiosInstance.interceptors.request.use(
 );
 
 /**
- * 401 응답 시 현재 경로를 redirectTo에 담아 로그인 페이지로 이동
+ * accessToken 만료 시 재발급받도록 로그인 페이지로 리다이렉션
+ * @Todo accessToken, refreshToken 저장 방식 수정 후 로직 추가
+ * refreshToken 여부 확인 후 재발급 or 로그인 페이지 리다이렉션 로직 추가
  */
 axiosInstance.interceptors.response.use(
   function (response) {
@@ -48,8 +43,9 @@ axiosInstance.interceptors.response.use(
   },
   async function (error) {
     if (error.response && error.response.status === 401) {
-      const redirectTo = encodeURIComponent(window.location.pathname);
-      window.location.href = `${ROUTE.login}?redirectTo=${redirectTo}`;
+      alert('세션이 만료되었습니다. 재로그인해주세요');
+      window.location.href = ROUTE.login;
+      localStorage.removeItem('accessToken');
     }
     return Promise.reject(error);
   }
