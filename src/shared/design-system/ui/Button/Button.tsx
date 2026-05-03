137eccb2ca2f4e25b92a91e8435917c05c2fa560
yoouyeon <jyeon.yoon59@gmail.com>
2026-05-03 20:33:51 +0900

    design: 디자인시스템 Button 컴포넌트와 스토리 추가

---
+++ src/shared/design-system/ui/Button/Button.tsx
@@ -0,0 +1,23 @@
+import { ButtonHTMLAttributes } from 'react';
+import * as S from './Button.styles';
+
+type ButtonVariant = 'primary' | 'secondary' | 'tertiary' | 'black';
+type ButtonSize = 'medium' | 'small' | 'xsmall';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+}
+
+function Button(props: ButtonProps) {
+  const { variant = 'primary', size = 'medium', children, ...rest } = props;
+
+  return (
+    <S.Button type="button" $variant={variant} $size={size} {...rest}>
+      {children}
+    </S.Button>
+  );
+}
+
+export { Button };
+export type { ButtonProps, ButtonVariant, ButtonSize };
