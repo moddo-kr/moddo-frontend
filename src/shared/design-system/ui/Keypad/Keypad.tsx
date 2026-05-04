9f22d9a39c8fcd330bf5d3f8f683451db22c3d63
yoouyeon <jyeon.yoon59@gmail.com>
2026-05-05 00:08:37 +0900

    design: 디자인시스템 Keypad 컴포넌트와 스토리 추가

---
+++ src/shared/design-system/ui/Keypad/Keypad.tsx
@@ -0,0 +1,54 @@
+import SvgBtnShortcut from '@/shared/assets/svgs/icon/BtnShortcut';
+import * as S from './Keypad.styles';
+
+type KeyValue =
+  | '0'
+  | '1'
+  | '2'
+  | '3'
+  | '4'
+  | '5'
+  | '6'
+  | '7'
+  | '8'
+  | '9'
+  | 'delete'
+  | 'cancel';
+
+interface KeypadProps {
+  onPress: (key: KeyValue) => void;
+}
+
+const KEYPAD_KEYS: KeyValue[] = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'cancel',
+  '0',
+  'delete',
+];
+
+function Keypad({ onPress }: KeypadProps) {
+  return (
+    <S.Grid>
+      {KEYPAD_KEYS.map((key) => (
+        <S.KeyButton key={key} type="button" onClick={() => onPress(key)}>
+          {key === 'delete' && <SvgBtnShortcut width={24} height={24} />}
+          {key === 'cancel' && <S.CancelText>취소</S.CancelText>}
+          {key !== 'delete' && key !== 'cancel' && (
+            <S.NumberText>{key}</S.NumberText>
+          )}
+        </S.KeyButton>
+      ))}
+    </S.Grid>
+  );
+}
+
+export { Keypad };
+export type { KeypadProps, KeyValue };
