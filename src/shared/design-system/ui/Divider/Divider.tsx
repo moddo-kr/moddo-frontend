9bc1b7648f2893190fc59ca7b668ed7d7b9da359
yoouyeon <jyeon.yoon59@gmail.com>
2026-05-06 03:10:27 +0900

    fix: Divider 접근성 속성 보강
    ref: https://github.com/moddo-kr/moddo-frontend/pull/42#discussion_r3190479547

--- src/shared/design-system/ui/Divider/Divider.tsx
+++ src/shared/design-system/ui/Divider/Divider.tsx
@@ -1,7 +1,7 @@
 import * as S from './Divider.styles';
 
 function Divider() {
-  return <S.Divider />;
+  return <S.Divider role="separator" aria-orientation="horizontal" />;
 }
 
 export { Divider };
