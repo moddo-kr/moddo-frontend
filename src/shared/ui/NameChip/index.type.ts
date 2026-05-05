7f4e0ef300086b6a49dae9da39d0b54de66c8b9e
yoouyeon <jyeon.yoon59@gmail.com>
2026-05-06 00:01:48 +0900

    refactor: NameChip 소비처를 디자인시스템 컴포넌트로 치환

--- src/shared/ui/NameChip/index.type.ts
+++
@@ -1,4 +0,0 @@
-export const nameChipVariants = ['orange', 'alternative', 'disabled'] as const;
-export const nameChipSizes = ['md', 'sm'] as const;
-export type NameChipVariant = (typeof nameChipVariants)[number];
-export type NameChipSize = (typeof nameChipSizes)[number];
