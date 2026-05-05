694c0cef40e7ca34b385897a6fb85f47dfee6f2b
yoouyeon <jyeon.yoon59@gmail.com>
2026-05-05 10:39:44 +0900

    wip: CurvedProgressBar 컴포넌트 추가

---
+++ src/_workspace/CurvedProgressBar/CurvedProgressBar.styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { getToken } from '@/shared/design-system';
+import SvgCrown from '@/shared/assets/svgs/icon/Crown';
+
+export const Container = styled.div`
+  position: relative;
+  width: 272px;
+`;
+
+export const ArcWrapper = styled.div`
+  width: 100%;
+`;
+
+export const CrownIcon = styled(SvgCrown)<{ $completed: boolean }>`
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  top: 120px;
+  right: 4px;
+  color: ${({ $completed }) =>
+    $completed ? getToken('fg.static-white') : getToken('fg.assistive')};
+`;
