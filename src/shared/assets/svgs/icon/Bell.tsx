2202f52c09f0ad237400d9b77cb997b995154eee
yoouyeon <jyeon.yoon59@gmail.com>
2026-05-10 16:01:48 +0900

    design: 아이콘 색깔을 상위에서 상속받도록 설정

--- src/shared/assets/svgs/icon/Bell.tsx
+++ src/shared/assets/svgs/icon/Bell.tsx
@@ -7,7 +7,7 @@ const SvgBell = (props: SVGProps<SVGSVGElement>) => (
     {...props}
     fill="none"
   >
-    <g stroke="#000" strokeLinecap="round" strokeWidth={1.5}>
+    <g stroke="currentColor" strokeLinecap="round" strokeWidth={1.5}>
       <path
         strokeLinejoin="round"
         d="M15.934 12.98a3 3 0 0 1-.457-1.59V7.226a6.477 6.477 0 0 0-12.954 0v4.162a3 3 0 0 1-.457 1.592L.978 14.72a1 1 0 0 0 .848 1.53h14.348a1 1 0 0 0 .848-1.53z"
