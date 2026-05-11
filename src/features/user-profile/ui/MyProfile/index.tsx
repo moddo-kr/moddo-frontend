b026a787f33e96fc33e4d10a52bb29749bfd14fc
yoouyeon <jyeon.yoon59@gmail.com>
2026-05-12 00:44:24 +0900

    refactor: MyProfile theme 의존 제거
    정보수정 버튼을 black/xsmall variant로 변경

--- src/features/user-profile/ui/MyProfile/index.tsx
+++ src/features/user-profile/ui/MyProfile/index.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from 'react-router';
-import { useTheme } from 'styled-components';
 import { useGetUserInfo } from '@/entities/auth/api/useGetUserInfo';
 import { ROUTE } from '@/shared/config/route';
 import { Button, ProfileImage } from '@/shared/design-system/ui';
@@ -8,7 +7,6 @@ import * as S from './index.styles';
 function MyProfile() {
   const { data: user } = useGetUserInfo();
   const navigate = useNavigate();
-  const theme = useTheme();
 
   // suspense로 감싸져 있긴 초기에 없는 경우의 에러를 방지하기 위해 null guard를 추가했습니다.
   // ref: https://github.com/moddo-kr/moddo-frontend/pull/30#discussion_r3068041167
@@ -24,13 +22,10 @@ function MyProfile() {
         {/* TODO: 디자인 시스템 정비 후 다시 디자인 확인이 필요합니다 (Opacity를 계속 쓰는지?) */}
         <S.UserEmail>{user.email}</S.UserEmail>
       </S.UserInfoWrapper>
-      {/* TODO: 현 피그마 디자인은 Chip이 Button으로 쓰이고 있는 상황이라 우선 button 컴포넌트 기준으로 구현했습니다. 디자인시스템 정리 후 다시 확인이 필요합니다! */}
       <Button
-        size="small"
+        variant="black"
+        size="xsmall"
         onClick={() => navigate(ROUTE.myEdit)}
-        style={{
-          backgroundColor: theme.color.semantic.background.normal.inverse,
-        }}
       >
         정보수정
       </Button>
