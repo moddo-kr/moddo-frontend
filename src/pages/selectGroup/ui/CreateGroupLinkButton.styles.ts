de994eb38d8dd8b16e726b775a99be67a01dc283
yoouyeon <jyeon.yoon59@gmail.com>
2026-05-13 21:39:43 +0900

    refactor: 시맨틱에 맞는 컬러 토큰으로 치환
    ref:
    https://github.com/moddo-kr/moddo-frontend/pull/53#discussion_r3234053608

--- src/pages/selectGroup/ui/CreateGroupLinkButton.styles.ts
+++ src/pages/selectGroup/ui/CreateGroupLinkButton.styles.ts
@@ -7,7 +7,7 @@ export const LinkButton = styled(Link)`
   @media (min-width: 600px) {
     font-size: 22px;
   }
-  background-color: ${getToken('fg.primary.normal')};
+  background-color: ${getToken('fill.primary.normal')};
   border-radius: ${getToken('radius.lg')};
   padding: ${getToken('padding.5')} ${getToken('padding.3')};
   cursor: pointer;
