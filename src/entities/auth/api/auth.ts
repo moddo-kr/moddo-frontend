41a246266134991816708035bd56f33d9ebcaaf7
yoouyeon <jyeon.yoon59@gmail.com>
2026-05-31 17:59:01 +0900

    remove: 게스트 로그인 로직 제거
    게스트 로그인 정책이 제거됨에 따라 남아있던 게스트 로그인 코드도 제거함

--- src/entities/auth/api/auth.ts
+++ src/entities/auth/api/auth.ts
@@ -1,21 +1,6 @@
 import axiosInstance from '@/shared/api/axios';
 import { AuthCheckResponse, User } from '../model/user.type';
 
-// CHECK - 게스트 토큰 정책 제거 가능성 있음
-export interface GuestTokenData {
-  accessToken: string;
-  refreshToken: string;
-  expiredAt: Date;
-  isMember: boolean;
-}
-
-export const getGuestToken = async (): Promise<GuestTokenData> => {
-  const response = await axiosInstance.get('/user/guest/token');
-  return response.data;
-};
-
-// ==========
-
 export const getAuth = async (): Promise<AuthCheckResponse> => {
   const response = await axiosInstance.get<AuthCheckResponse>('/auth/check');
   return response.data;
--- src/mocks/handlers/auth.ts
+++ src/mocks/handlers/auth.ts
@@ -2,16 +2,6 @@ import { User } from '@/entities/auth/model/user.type';
 import { http, HttpResponse, passthrough } from 'msw';
 
 const authHandlers = [
-  http.get('/api/v1/user/guest/token', ({ request }) => {
-    const isMocked = request.headers.get('X-Mock-Request');
-    if (!isMocked || isMocked !== 'true') return passthrough();
-    return HttpResponse.json({
-      accessToken: import.meta.env.VITE_MOCK_ACCESS_TOKEN,
-      refreshToken: import.meta.env.VITE_MOCK_REFRESH_TOKEN,
-      expiredAt: '2025-03-06 14:17:26',
-      isMember: false,
-    });
-  }),
   http.get('/api/v1/user/auth/check', ({ request }) => {
     const isMocked = request.headers.get('X-Mock-Request');
     if (!isMocked || isMocked !== 'true') return passthrough();
