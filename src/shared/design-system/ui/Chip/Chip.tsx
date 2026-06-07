d30f19ca1cb696f673f6bf47e9e90eda2643626a
yoouyeon <jyeon.yoon59@gmail.com>
2026-06-07 20:05:05 +0900

    refactor: Chip black variant HACK 주석 제거

--- src/shared/design-system/ui/Chip/Chip.tsx
+++ src/shared/design-system/ui/Chip/Chip.tsx
@@ -1,7 +1,6 @@
 import type { MouseEventHandler } from 'react';
 import * as S from './Chip.styles';
 
-// HACK : ExpenseTimelineContent 에서 사용하는 Chip이 정의되어 있지 않아서 임의로 black variant를 추가함.
 type ChipVariant = 'selected' | 'unselected' | 'disabled' | 'red' | 'black';
 type ChipSize = 'm' | 's';
 
